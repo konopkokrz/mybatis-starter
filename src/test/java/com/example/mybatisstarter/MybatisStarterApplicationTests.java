0c8b3e5f420ad8d079adc8ab6b9eb0421d063903
Krzysztof Konopko <krzysztof@MacBook-Pro-Krzysztof.local>
2022-01-17 13:36:06 +0100

    Initial commit

---
+++ src/test/java/com/example/mybatisstarter/MybatisStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mybatisstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
